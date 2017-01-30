3d561551d6b392f1ddca4902e4395eedafc8ad92
Curtis McCully <cmccully@lcogt.net>
2017-01-30 12:02:34 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Transient Host Exchange
+
+An interface to collate and interact with transient host galaxy information
+
+author: Curtis McCully (cmccully@lco.global)
+
+January 2017
+"""
+from setuptools import setup
+
+setup(name='thex',
+      author=['Curtis McCully'],
+      author_email=['cmccully@lco.global'],
+      version=0.1,
+      packages=['thex'],
+      setup_requires=[],
+      install_requires=['numpy', 'astropy', 'django'],
+      tests_require=[])
